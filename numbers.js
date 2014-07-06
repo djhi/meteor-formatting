7f37d8cc7a66617fc3a7d11537c531e7f11fee3d
djhi <gildas.garcia@gmail.com>
2014-07-06 12:24:54 +0200

    Meilleure détection des options

--- numbers.js
+++ numbers.js
@@ -1,12 +1,14 @@
 _.extend(MeteorFormatting, {
   number: function(value, keywords) {
     var numberForFormatting = 0;
+    var isNumber = (keywords && keywords.hash) ? keywords.hash.isNumber : false;
+    var format = (keywords && keywords.hash) ? keywords.hash.format : false;
 
     if (typeof value === 'number' || (typeof value === 'string' && value.length > 0)) {
       numberForFormatting = value;
 
       if (typeof value === 'string') {
-        if (keywords.hash.isNumber) {
+        if (isNumber) {
           numberForFormatting = parseFloat(value);
         } else {
           numberForFormatting = numeral().unformat(value);
@@ -14,16 +16,17 @@ _.extend(MeteorFormatting, {
       }
     }
 
-    return numeral(numberForFormatting).format(keywords.hash.format || '0.00');
+    return numeral(numberForFormatting).format(format || '0.00');
   },
   currencyNoSymbol: function(value, keywords) {
     var numberForFormatting = 0;
+    var isNumber = (keywords && keywords.hash) ? keywords.hash.isNumber : false;
 
     if (typeof value === 'number' || (typeof value === 'string' && value.length > 0)) {
       var numberForFormatting = value;
 
       if (typeof value === 'string') {
-        if (keywords.hash.isNumber) {
+        if (isNumber) {
           numberForFormatting = parseFloat(value);
         } else {
           numberForFormatting = numeral().unformat(value);
@@ -35,12 +38,14 @@ _.extend(MeteorFormatting, {
   },
   currency: function(value, keywords) {
     var numberForFormatting = 0;
+    var isNumber = (keywords && keywords.hash) ? keywords.hash.isNumber : false;
+    var symbolAtEnd = (keywords && keywords.hash) ? keywords.hash.symbolAtEnd : false;
 
     if (typeof value === 'number' || (typeof value === 'string' && value.length > 0)) {
       var numberForFormatting = value;
 
       if (typeof value === 'string') {
-        if (keywords.hash.isNumber) {
+        if (isNumber) {
           numberForFormatting = parseFloat(value);
         } else {
           numberForFormatting = numeral().unformat(value);
@@ -48,17 +53,18 @@ _.extend(MeteorFormatting, {
       }
     }
 
-    return numeral(numberForFormatting).format((!keywords.hash.symbolAtEnd ? '$' : '') + '0.00' + (keywords.hash.symbolAtEnd ? '$' : ''));
+    return numeral(numberForFormatting).format((!symbolAtEnd ? '$' : '') + '0.00' + (symbolAtEnd ? '$' : ''));
   },
 
   percentage: function(value, keywords) {
     var numberForFormatting = 0;
+    var isNumber = (keywords && keywords.hash) ? keywords.hash.isNumber : false;
 
     if (typeof value === 'number' || (typeof value === 'string' && value.length > 0)) {
       var numberForFormatting = value;
 
       if (typeof value === 'string') {
-        if (keywords.hash.isNumber) {
+        if (isNumber) {
           numberForFormatting = parseFloat(value);
         } else {
           numberForFormatting = numeral().unformat(value);
